a5ce6197a7e18c8453e5b95c138e3da30d3e5b54
JAN_ADRIAN_SOSA_CORDERO <a.22308051281317@cbtis128.edu.mx>
2025-03-11 15:18:22 +0000

    Examen1317

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List View Builder'),
+          backgroundColor: Colors.deepPurple, // Color de la AppBar
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            _buildListItem('Apple', 'A'),
+            _buildListItem('Food', 'F'),
+            _buildListItem('Product', 'P'),
+            _buildListItem('Item', 'I'),
+            _buildListItem('List View', 'L'),
+            _buildListItem('New App', 'N'),
+            _buildListItem('Design', 'D'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'JAN ADRIAN SOSA CORDERO 22308051281317',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.deepPurple, // Color del FAB
         ),
       ),
+    );
+  }
+
+  Widget _buildListItem(String title, String iconLetter) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.deepPurple, // Color del CircleAvatar
+        child: Text(
+          iconLetter,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      title: Text(title),
     );
   }
 }
